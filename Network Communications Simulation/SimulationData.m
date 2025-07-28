a111a7f966de9cb8c2ae2920e0dd7581bc2bd708
Oliver Beresford <oggynaut@icloud.com>
2025-07-28 13:33:59 -0400

    Updated class with more properties

--- Network Communications Simulation/SimulationData.m
+++ Network Communications Simulation/SimulationData.m
@@ -1,16 +1,21 @@
 classdef SimulationData
     properties 
-        sourcePower(1, 1) {mustBeNumeric} = 1;
+        sourcePower(1, 1) {mustBeNumeric};
         receiver {mustBeVector} = [0 0];
-        alpha(1, 1) {mustBeNumeric} = 4;
-        A(1, 1) {mustBeNumeric} = 1;
-        fadingMean(1, 1) {mustBeNumeric} = 1;
-        noiseP(1, 1) {mustBeNumeric} = 1e-9;
+        alpha(1, 1) {mustBeNumeric};
+        A(1, 1) {mustBeNumeric};
+        fadingMean(1, 1) {mustBeNumeric};
+        noiseP(1, 1) {mustBeNumeric};
         baseStations(:, 2) {mustBeMatrix, mustBeNumeric}
         stationCount(1, 1) {mustBeNumeric}
-        penetrationLoss(1, 1) {mustBeNumeric, mustBeInRange(penetrationLoss, 0, 1)} = 1;
+        penetrationLoss(1, 1) {mustBeNumeric, mustBeInRange(penetrationLoss, 0, 1)};
         avenues {mustBeMatrix}
         streets {mustBeMatrix}
+        lambdaBase(1, 1) {mustBeNumeric}
+        lambdaAve(1, 1) {mustBeNumeric}
+        lambdaSt(1, 1) {mustBeNumeric}
+        useNLOS(1, 1) {mustBeNumericOrLogical}
+        useDiffraction(1, 1) {mustBeNumericOrLogical}
     end
     methods
         function obj = SimulationData(options)
@@ -22,19 +27,26 @@ classdef SimulationData
                 options.alpha(1, 1) {mustBeNumeric} = 4;
                 options.A(1, 1) {mustBeNumeric} = 1;
                 options.fadingMean(1, 1) {mustBeNumeric} = 1;
-                options.noiseP(1, 1) {mustBeNumeric} = 1e-9;
+                options.noiseP(1, 1) {mustBeNumeric} = 0;
                 options.baseStations(:, 2) {mustBeMatrix, mustBeNumeric} = [];
                 options.stationCount(1, 1) {mustBeNumeric} = 0;
                 options.penetrationLoss(1, 1) {mustBeNumeric, mustBeInRange(options.penetrationLoss, 0, 1)} = 1;
                 options.avenues {mustBeMatrix} = [];
                 options.streets {mustBeMatrix} = [];
-                options.size(1, 1) {mustBeNumeric} = 10;
+                options.size(1, 1) {mustBeNumeric} = 50;
                 options.lambdaBase(1, 1) {mustBeNumeric} = 0.1;
                 options.lambdaAve(1, 1) {mustBeNumeric} = 1;
                 options.lambdaSt(1, 1) {mustBeNumeric} = 1;
                 options.plotGraph(1, 1) {mustBeNumericOrLogical} = false;
+                options.useNLOS(1, 1) {mustBeNumericOrLogical} = false;
+                options.useDiffraction(1, 1) {mustBeNumericOrLogical} = false;
             end
             % Setting the object's properties
+            obj.lambdaBase = options.lambdaBase;
+            obj.lambdaAve = options.lambdaAve;
+            obj.lambdaSt = options.lambdaSt;
+            obj.useNLOS = options.useNLOS;
+            obj.useDiffraction = options.useDiffraction;
             obj.sourcePower = options.sourcePower;
             obj.receiver = options.receiver;
             obj.alpha = options.alpha;
