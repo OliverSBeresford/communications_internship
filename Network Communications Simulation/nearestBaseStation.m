c7298899580f34fefe2fd2ec3e309a893327d235
Oliver Beresford <oggynaut@icloud.com>
2025-07-28 18:45:18 -0400

    Updated nearestBaseStation to reflect new data structure

--- Network Communications Simulation/nearestBaseStation.m
+++ Network Communications Simulation/nearestBaseStation.m
@@ -1,12 +1,12 @@
-function [station, closestIndex] = nearestBaseStation(receiver, baseStations)
-    % A vector with the distance from receiver to every base station
+function [station, closestIndex] = nearestBaseStation(data)
+    % A vector with the distance from data.receiver to every base station
     distances = sqrt(...
-        (baseStations(:, 1) - receiver(1)) .^ 2 + ...
-        (baseStations(:, 2) - receiver(2)) .^ 2);
+        (data.baseStations(:, 1) - data.receiver(1)) .^ 2 + ...
+        (data.baseStations(:, 2) - data.receiver(2)) .^ 2);
     
     % Get the index of the closest base station
     [~, closestIndex] = min(distances);
     
     % Coordinates of the base station
-    station = baseStations(closestIndex);
+    station = data.baseStations(closestIndex);
 end
