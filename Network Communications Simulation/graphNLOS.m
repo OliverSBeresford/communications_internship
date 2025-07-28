59e9e625995b6b1676e0979deede51ad718f4d32
Oliver Beresford <oggynaut@icloud.com>
2025-07-28 15:07:36 -0400

    This file graph the 1d network and multiple 2d networks with NLOS interference, with each one having a different K value (penetration loss)

---
+++ Network Communications Simulation/graphNLOS.m
@@ -0,0 +1,65 @@
+simulations = 1e4;
+
+kValues = 10.^(-1:-1:-5);
+colors = [0 0 0; 0.25 0 0; 0.5 0 0; 0.75 0 0; 1 0 0];
+
+% This is where all the SINR results are stored
+results = zeros(length(kValues) + 1, simulations);
+
+% Initialize the object that stores the data in intermediate steps
+% Provide all the parameters to SimulationData
+data = SimulationData( ...
+    useNLOS=false, ...
+    size=50, ...
+    lambdaBase=0.1, ...
+    lambdaAve=1, ...
+    lambdaSt=1, ...
+    plotGraph=false, ...
+    sourcePower=1, ...
+    alpha=4, ...
+    A=1, ...
+    fadingMean=1, ...
+    noisePower=0, ...
+    doManhattan=true ...
+);
+
+% Calculate SINR (simulations) times for the 1d network
+for j = 1:simulations
+    data.runManhattan();
+    result = SINR(data);
+    results(1, j) = 10 * log10(result);
+end
+
+% Plot the CCDF graph for 1d
+figure(1)
+hold on
+
+[x, y] = CCDF(results(1, :));
+
+% Plot the 1d graph
+plot(x, y, Color='b');
+
+% Plot each line for different K values
+data.useNLOS = true;
+for ii = 1:length(kValues)
+    % Set the new K value each iteration
+    data.penetrationLoss = kValues(ii);
+    for j = 1:simulations
+        data.runManhattan();
+        result = SINR(data);
+        results(ii + 1, j) = 10 * log10(result);
+    end
+    
+    % Plot the CCDF graph for this K value
+    [x, y] = CCDF(results(ii + 1, :));
+
+    % Plot the graph
+    plot(x, y, Color=colors(ii, :));
+end
+
+% Label the graph
+title('Coverage probability CCDF');
+xlabel('\theta');
+ylabel('Probability');
+
+hold off
