5cc9ff70487f53668aedcd53cd34d4bce84f08c7
Oliver Beresford <oggynaut@icloud.com>
2025-08-05 13:04:35 -0400

    File now displays the roads by just displaying all the points along them.

--- Network Communications Simulation/dispPoints.m
+++ Network Communications Simulation/dispPoints.m
@@ -5,8 +5,8 @@ GT = readgeotable("map.osm", "Layer","points");
 projCRS = projcrs(32619);
 
 % 3. Extract latitude and longitude from each geolineshape and project
-x_coords = cell(1, 2503);
-y_coords = cell(1, 2503);
+x_coords = zeros(1, 2503);
+y_coords = zeros(1, 2503);
 
 for i = 1:height(GT)
     % Extract latitude and longitude from the current geolineshape
@@ -21,14 +21,14 @@ for i = 1:height(GT)
     [x, y] = projfwd(projCRS, lat, lon);
 
     % Store the projected coordinates
-    x_coords{i} = x;
-    y_coords{i} = y;
+    x_coords(i) = x;
+    y_coords(i) = y;
 end
 
 % Plot the projected lines
 hold on;
 
-scatter(cell2mat(x_coords), cell2mat(y_coords), Marker=".", Color=[0 0 0]);
+scatter(x_coords, y_coords, Marker=".", Color=[0 0 0]);
 
 hold off;
 
