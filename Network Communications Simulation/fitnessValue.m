050bfd4042f255208fee046e2104e892aaafed89
Oliver Beresford <oggynaut@icloud.com>
2025-08-11 13:17:00 -0400

    Added file to calculate the fitness value a map with a given set of base stations

---
+++ Network Communications Simulation/fitnessValue.m
@@ -0,0 +1,32 @@
+function total = fitnessValue(data, computationNodes, thresholdDB)
+    total = 0;
+    
+    % Going through each avenue and looking at 500 points
+    for ave = data.avenues
+        for y = linspace(-data.size/2, data.size/2, computationNodes)
+            % Checking SINR for a user at this point
+            data.receiver = [ave, y];
+            sinr = 10 * log10(SINR(data));
+
+            % Indicator function: increase fitness if SINR > threshold
+            if sinr > thresholdDB
+                total = total + 1;
+            end
+        end
+    end
+
+    % Going through each street and looking at 500 points
+    for st = data.streets
+        for x = linspace(-data.size/2, data.size/2, computationNodes)
+            % Checking SINR for a user at this point
+            data.receiver = [x, st];
+            sinr = 10 * log10(SINR(data));
+
+            % Indicator function: increase fitness if SINR > threshold
+            if sinr > thresholdDB
+                total = total + 1;
+            end
+        end
+    end
+
+end
