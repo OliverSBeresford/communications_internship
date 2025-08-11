2297d8c108c967c591cd9de0822a6af29b1a73dc
Oliver Beresford <oggynaut@icloud.com>
2025-08-11 10:57:46 -0400

    Made manhattan function more tailored to the SimulationData class

--- Network Communications Simulation/manhattan.m
+++ Network Communications Simulation/manhattan.m
@@ -1,53 +1,53 @@
-function [avenues, streets, allStations, stationCount, aveCounts, stCounts] = manhattan(size, lambdaBase, lambdaStreet, lambdaAvenue)
+function manhattan(data)
     % Homogeneous Poisson-Point Processes
-    numAvenues = poissrnd(size * lambdaAvenue);
-    numStreets = poissrnd(size * lambdaStreet);
+    numAvenues = poissrnd(data.size * data.lambdaAve);
+    numStreets = poissrnd(data.size * data.lambdaSt);
     
     % Uniformly distribute the avenues and streets
-    avenues = rand(1, numAvenues) .* size - (size / 2);
-    streets = [0 rand(1, numStreets) .* size - (size / 2)];
+    data.avenues = rand(1, numAvenues) .* data.size - (data.size / 2);
+    data.streets = [0 rand(1, numStreets) .* data.size - (data.size / 2)];
     
     % Number of base stations on each avenue, then on each street
-    aveCounts = poissrnd(lambdaBase * size, 1, numAvenues);
-    stCounts = poissrnd(lambdaBase * size, 1, numStreets + 1);
+    data.aveCounts = poissrnd(data.lambdaBase * data.size, 1, numAvenues);
+    data.stCounts = poissrnd(data.lambdaBase * data.size, 1, numStreets + 1);
 
     % Create matrix for all the stations
-    stationCount = sum(aveCounts) + sum(stCounts);
-    allStations = zeros(stationCount, 2);
+    data.stationCount = sum(data.aveCounts) + sum(data.stCounts);
+    data.baseStations = zeros(data.stationCount, 2);
     
     index = 1;
 
     % Creating base stations
     for ii = 1:numAvenues
         % Row 1 is avenues, column is the base station count for that ave
-        thisAveCount = aveCounts(ii);
+        thisAveCount = data.aveCounts(ii);
 
         % Calculating #stations and creating matrix with (x, y) coordinates
         stations = zeros(thisAveCount, 2);
 
         % Their y coordinate is random and uniformly distrubuted
-        stations(:, 2) = rand(1, thisAveCount) .* size - size / 2;
+        stations(:, 2) = rand(1, thisAveCount) .* data.size - data.size / 2;
         % Their x coordinates are all equal to the avenue's
-        stations(:, 1) = avenues(ii);
+        stations(:, 1) = data.avenues(ii);
 
         % Updating main matrix
-        allStations(index:index + thisAveCount - 1, :) = stations;
+        data.baseStations(index:index + thisAveCount - 1, :) = stations;
         index = index + thisAveCount;
     end
     for ii = 1:numStreets + 1
         % Row 2 is streets, column is the base station count for that st
-        thisStCount = stCounts(ii);
+        thisStCount = data.stCounts(ii);
 
         % Calculating #stations and creating matrix with (x, y) coordinates
         stations = zeros(thisStCount, 2);
 
         % Their x coordinates are random and uniformly distrubuted
-        stations(:, 1) = rand(1, thisStCount) .* size - size / 2;
+        stations(:, 1) = rand(1, thisStCount) .* data.size - data.size / 2;
         % Their y coordinates are all equal to the avenue's
-        stations(:, 2) = streets(ii);
+        stations(:, 2) = data.streets(ii);
 
         % Updating main matrix
-        allStations(index:index + thisStCount - 1, :) = stations;
+        data.baseStations(index:index + thisStCount - 1, :) = stations;
         index = index + thisStCount;
     end
 end
