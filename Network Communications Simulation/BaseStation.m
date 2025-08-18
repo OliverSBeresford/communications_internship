258493068e1879ba250184625057d570a84281e1
Oliver Beresford <oggynaut@icloud.com>
2025-08-18 13:30:02 -0400

    Add constructor

--- Network Communications Simulation/BaseStation.m
+++ Network Communications Simulation/BaseStation.m
@@ -5,4 +5,14 @@ classdef BaseStation < handle
         z(1, 1) {mustBeNumeric};
         power(1, 1) {mustBeNumeric} = 1;
     end
+
+    methods
+        % Constructor function
+        function obj = BaseStation(x, y, z, power)
+            obj.x = x;
+            obj.y = y;
+            obj.z = z;
+            obj.power = power;
+        end
+    end
 end
